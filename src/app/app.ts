afe810b7721b23a5056de6fa4626c51d4e94adaa
2NingGiselleCachorra <rjoao@alunos.afya.com.br>
2026-06-15 19:34:08 -0400

    DELETING - HeaderComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Pokemon } from './components/pokemon/pokemon';
-import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Pokemon, Header],
+  imports: [RouterOutlet, Pokemon],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

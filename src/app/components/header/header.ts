952486985385f95136bd52ba36cf4f97e2a6f052
2NingGiselleCachorra <rjoao@alunos.afya.com.br>
2026-06-18 17:55:33 -0400

    HEADER ALMOST FINESHED

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.css',
 })
 export class Header {
+  indexPokemon = 1;
+  pokemonSprite = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
 
+changePokemonImg() {
+  
+  this.indexPokemon++;
+  this.pokemonSprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.indexPokemon}.png`;
+  }
 }
